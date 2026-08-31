94bd5294a99760258a6f27ef4425fa2aa84c3ab0
francis-chung <francis.cy.chung@gmail.com>
2026-08-31 08:59:30 -0400

    fix goofy u64 problems in WAL

--- src/wal.rs
+++ src/wal.rs
@@ -71,12 +71,16 @@ pub fn replay_from_bytes(bytes: &[u8], store: &mut Db) -> io::Result<u64> {
 }
 
 pub async fn replay_from_position(path: &str, pos: u64, store: &mut Db) -> io::Result<u64> {
-    let mut bytes = match tokio::fs::read(path).await {
+    let bytes = match tokio::fs::read(path).await {
         Ok(b) => b, 
         Err(e) if e.kind() == io::ErrorKind::NotFound => return Ok(0), 
         Err(e) => return Err(e),
     };
-    bytes = bytes[pos as usize..].to_vec();
+    let pos = pos as usize;
+    if pos > bytes.len() {
+        return Err(io::Error::new(io::ErrorKind::InvalidData, "Snapshot position beyond WAL length"));
+    }
+    let bytes = &bytes[pos as usize..];
     replay_from_bytes(&bytes, store)
 }
 
